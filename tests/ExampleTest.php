6aaa548f902631af273432f06aad16b0f04e9c7c
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-05-04 16:32:17 -0400

    Added example test.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ *
+ * @author Sam Schmidt
+ * @date 2015-05-04
+ * @company Linus Shops
+ */
+
+class ExampleTest extends PHPUnit_Framework_TestCase{
+    public function testExample()
+    {
+
+    }
+}
